fb7ff8f11aeff5e1ca99488706484cf40b5eba5f
siwar <siwarsaidane@gmail.com>
2025-03-12 01:28:49 +0100

    fix grammar

--- lib/screens/my_profile_detail_screen.dart
+++ lib/screens/my_profile_detail_screen.dart
@@ -5,7 +5,6 @@ import 'package:datingapp/screens/setting_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
